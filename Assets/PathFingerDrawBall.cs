3ab0549c15b358bade238d4a3b39bf6d9e78d2b6
Fredrik Wallander <f.m.wallander@gmail.com>
2021-02-12 20:18:14 +0100

    Continuation of finger path move

--- Assets/PathFingerDrawBall.cs
+++ Assets/PathFingerDrawBall.cs
@@ -1,18 +1,25 @@
-using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PathFingerDrawBall : MonoBehaviour
-{
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
+public class PathFingerDrawBall : MonoBehaviour{
+    public LineRenderer lr;
+    public List<Vector3> vectorPoints;
     void Update()
     {
-        
+        if (Input.GetMouseButtonDown(0)){
+            vectorPoints = new List<Vector3>();
+        }
+        if (Input.GetMouseButton(0)){
+            Ray myRay = Camera.main.ScreenPointToRay(Input.mousePosition);
+            if (Physics.Raycast(myRay, out var hitInfo)){
+                if (Vector3.Distance(this.transform.position, hitInfo.point) > 1f){
+                    vectorPoints.Add(hitInfo.point);
+                }
+            }
+        }
+
+        if (Input.GetMouseButtonUp(0)){
+            // start moving the ball, finish recording input.
+        }
     }
 }
